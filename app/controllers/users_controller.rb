b35adfa49ca482d25900c3dc1bb8454d3df007cc
a.tango <a.tango@leihauoli.com>
2016-09-04 22:08:03 +0900

    11章　11.4.2まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-  end
+     @user = User.find(params[:id])
+     @microposts = @user.microposts.paginate(page: params[:page])
+   end
 
   def new
     @user = User.new
@@ -56,13 +57,13 @@ class UsersController < ApplicationController
     # beforeフィルター
 
     # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    #def logged_in_user
+      #unless logged_in?
+        #store_location
+        #flash[:danger] = "Please log in."
+        #redirect_to login_url
+    #  end
+    #end
 
     # 正しいユーザーかどうか確認
     def correct_user
